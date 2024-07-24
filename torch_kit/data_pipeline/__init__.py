d08444025fbe8a34bd6da7396c7d2b55ba067815
Meriem30 <m.arbaoui@esi-sba.dz>
2024-07-24 10:45:27 +0200

    add __init__.py for data_pipeline

---
+++ torch_kit/data_pipeline/__init__.py
@@ -0,0 +1,11 @@
+from ..factory import Factory
+from .transform import *
+from .dataset import *
+
+global_data_transform_factory = Factory()
+
+
+def append_transforms_to_dc(dc, model_evaluator=None) -> None:
+    constructor = global_data_transform_factory.get(dc.dataset_type)
+    assert constructor is not None
+    return constructor(dc=dc, model_evaluator=model_evaluator)
