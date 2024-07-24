622b9598c550b988da60e6e0748ab72451215290
Meriem30 <m.arbaoui@esi-sba.dz>
2024-07-24 11:23:51 +0200

    add ml_type.py in ./torch_kit

---
+++ torch_kit/ml_type.py
@@ -0,0 +1,67 @@
+from enum import StrEnum, auto
+
+"""
+    For each class:
+    Set a unique value for each enumeration member automatically using auto()
+    Ensure that the enumeration members are strings using StrEnum => MachineLearningPhase.Training := "Training"
+    StrEnum & auto =>  the enumeration members and their values are the same (self-explanatory)
+"""
+
+class MachineLearningPhase(StrEnum):
+    Training = auto()
+    Validation = auto()
+    Test = auto()
+
+
+class EvaluationMode(StrEnum):
+    Training = auto()
+    Test = auto()
+    TestWithGrad = auto()
+
+
+class ModelType(StrEnum):
+    Classification = auto()
+    Detection = auto()
+    TextGeneration = auto()
+
+
+class DatasetType(StrEnum):
+    Vision = auto()
+    Text = auto()
+    Graph = auto()
+    Audio = auto()
+    CodeText = auto()
+    Unknown = auto()
+
+
+class TransformType(StrEnum):
+    ExtractData = auto()
+    InputText = auto()
+    Input = auto()
+    RandomInput = auto()
+    InputBatch = auto()
+    Target = auto()
+    TargetBatch = auto()
+
+
+class ExecutorHookPoint(StrEnum):
+    BEFORE_EXECUTE = auto()
+    AFTER_EXECUTE = auto()
+    BEFORE_EPOCH = auto()
+    AFTER_EPOCH = auto()
+    MODEL_FORWARD = auto()
+    BEFORE_FETCH_BATCH = auto()
+    AFTER_FETCH_BATCH = auto()
+    BEFORE_BATCH = auto()
+    AFTER_BATCH = auto()
+    AFTER_VALIDATION = auto()
+
+
+class StopExecutingException(Exception):
+    pass
+
+
+class IterationUnit(StrEnum):
+    Batch = auto()
+    Epoch = auto()
+    Round = auto()
