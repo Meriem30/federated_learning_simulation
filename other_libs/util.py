92be7fa4e45c90ab325253b11ff9ca94e8fbba4c
Meriem30 <m.arbaoui@esi-sba.dz>
2024-07-25 16:06:36 +0200

    add util.py in ./other_libs

---
+++ other_libs/util.py
@@ -0,0 +1,38 @@
+import time
+import traceback
+from typing import Callable, List, Tuple
+
+from log import get_logger
+
+
+def retry_operation(
+    operation: Callable, retry_times: int, retry_sleep_time: float, *args, **kwargs
+) -> Tuple[bool, None]:
+    """
+        Attempt a specific operation until it succeeds or a max nbr of retries is reached
+    """
+    for _ in range(retry_times):
+        try:
+            res = operation(*args, **kwargs)
+            if res[0]:
+                return res
+        except Exception as e:
+            get_logger().error("catch exception:%s", e)
+            get_logger().error("traceback:%s", traceback.format_exc())
+        get_logger().error("operation failed,retry after %s seconds", retry_sleep_time)
+        time.sleep(retry_sleep_time)
+    return False, None
+
+
+def readlines(file_path: str) -> List[str]:
+    """
+        Read all lines form a file and return them as a list of strings
+    """
+    lines = []
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            lines += f.readlines()
+    except Exception:
+        with open(file_path, "r", encoding="utf-8-sig") as f:
+            lines += f.readlines()
+    return lines
