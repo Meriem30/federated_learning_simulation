cd3c8bc7f9aab1df5a116b7f9e982a6f4d146e88
Meriem30 <m.arbaoui@esi-sba.dz>
2024-07-30 11:28:24 +0200

    add perfprmance_metric.py to include all metrics in ./torch_kit/metrics

---
+++ torch_kit/metrics/perfromance_metric.py
@@ -0,0 +1,54 @@
+import time
+from typing import Any
+
+import torch
+
+from ..ml_type import ModelType
+from .acc_metric import AccuracyMetric
+from .auc_metric import AUROCMetric
+from .dataloader_profiler import DataloaderProfiler
+from .f1_score_metric import F1Metric
+from .grad_metric import GradMetric
+from .learning_rate_metric import LearningRateMetric
+from .loss_metric import LossMetric
+from .metric import Metric
+
+
+class PerformanceMetric(Metric):
+    def __init__(
+        self,
+        executor,
+        profile: bool = False,
+        extra_metrics: bool = False,
+        use_grad_norm: bool = False,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.loss_metric = LossMetric()
+        if executor.running_model_evaluator.model_type == ModelType.Classification:
+            self.accuracy_metric = AccuracyMetric()
+            if extra_metrics:
+                self.f1_metric = F1Metric()
+                self.auc_metric = AUROCMetric()
+        self.__epoch_time_point: float = time.time()
+        self.__last_epoch: None | int = None
+        if use_grad_norm:
+            self.grad_metric = GradMetric()
+        if profile:
+            self.dataloader_profiler = DataloaderProfiler()
+        if hasattr(executor, "train"):
+            self.lr_metric = LearningRateMetric()
+
+    def _before_epoch(self, **kwargs: Any) -> None:
+        self.__epoch_time_point = time.time()
+
+    def _after_epoch(self, epoch: int, **kwargs: Any) -> None:
+        self.__last_epoch = epoch
+        self._set_epoch_metric(epoch, "duration", time.time() - self.__epoch_time_point)
+
+    def get_loss(self, epoch: int, to_item: bool = True) -> float | torch.Tensor:
+        return self.get_epoch_metric(epoch=epoch, name="loss", to_item=to_item)
+
+    def get_last_loss(self) -> float | torch.Tensor:
+        assert self.__last_epoch is not None
+        return self.get_loss(epoch=self.__last_epoch)
