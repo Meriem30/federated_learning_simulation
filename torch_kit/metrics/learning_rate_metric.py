2eb5cad4a0bd875ccb902546792d94ba0dc48295
Meriem30 <m.arbaoui@esi-sba.dz>
2024-07-30 11:15:22 +0200

    add learning_rate.py ./torch_kit/metrics

---
+++ torch_kit/metrics/learning_rate_metric.py
@@ -0,0 +1,13 @@
+from .metric import Metric
+
+
+class LearningRateMetric(Metric):
+    def _before_batch(self, executor, batch_index, **kwargs) -> None:
+        optimizer = executor.get_optimizer()
+        assert optimizer is not None
+        self._set_batch_metric(
+            batch_index=batch_index,
+            name="learning_rate",
+            data=[group["lr"] for group in optimizer.param_groups],
+        )
+        
