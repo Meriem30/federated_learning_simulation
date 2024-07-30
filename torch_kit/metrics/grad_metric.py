d8d3122879966ebff724654c8a1ab2bd64bd2781
Meriem30 <m.arbaoui@esi-sba.dz>
2024-07-30 10:46:56 +0200

    add grad_metric.py in ./torch_kit/metrics

---
+++ torch_kit/metrics/grad_metric.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+import torch
+
+from .metric import Metric
+
+
+class GradMetric(Metric):
+    # Initialize a param to store the model parameters from the previous epoch
+    __prev_parameter_list: Any = None
+
+    def _before_epoch(self, executor, **kwargs: Any) -> None:
+        if self.__prev_parameter_list is None:
+            self.__prev_parameter_list = executor.model_util.get_parameter_list()
+
+    def _after_epoch(self, epoch, executor, **kwargs) -> None:
+        parameter_list = executor.model_util.get_parameter_list()
+        self._set_epoch_metric(
+            epoch,
+            "grad_norm",
+            torch.norm(
+                parameter_list - self.__prev_parameter_list.to(executor.device)
+            ).item(),
+        )
+        self.__prev_parameter_list = parameter_list
