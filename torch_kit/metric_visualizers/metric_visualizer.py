7168c140d9bb3459ed3bd31813e4b53044160ba3
Meriem30 <m.arbaoui@esi-sba.dz>
2024-07-30 11:38:02 +0200

    add metric_visualizer.py in ./torch_kit/metric_visualizers

---
+++ torch_kit/metric_visualizers/metric_visualizer.py
@@ -0,0 +1,24 @@
+from ..hook import Hook
+
+
+class MetricVisualizer(Hook):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(stripable=True, **kwargs)
+        self._prefix: str = ""
+        self._data_dir: None | str = None
+
+    def set_data_dir(self, data_dir: str) -> None:
+        self._data_dir = data_dir
+
+    def set_prefix(self, prefix: str) -> None:
+        self._prefix = prefix
+
+    @property
+    def data_dir(self) -> str:
+        assert self._data_dir is not None
+        return self._data_dir
+
+    @property
+    def prefix(self) -> str:
+        return self._prefix
+    
