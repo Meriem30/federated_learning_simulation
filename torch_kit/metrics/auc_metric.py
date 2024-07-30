e2095af41fe322182483427c3a5f9890a799dd73
Meriem30 <m.arbaoui@esi-sba.dz>
2024-07-30 11:24:33 +0200

    add auc_metric.py in ./torch_kit/metrics

---
+++ torch_kit/metrics/auc_metric.py
@@ -0,0 +1,38 @@
+from typing import Any
+
+from torchmetrics.classification import MulticlassAUROC, MultilabelAUROC
+
+from .classification_metric import ClassificationMetric
+
+
+class AUROCMetric(ClassificationMetric):
+    __auroc: None | MulticlassAUROC | MultilabelAUROC = None
+
+    @property
+    def auroc(self):
+        assert self.__auroc is not None
+        return self.__auroc
+
+    def _before_epoch(self, **kwargs: Any) -> None:
+        self.__auroc = None
+
+    def _after_batch(self, result: dict, **kwargs: Any) -> None:
+        if self.__auroc is None:
+            executor = kwargs["executor"]
+            assert executor.dataset_collection.label_number > 0
+            with executor.device:
+                if executor.dataset_collection.is_mutilabel():
+                    self.__auroc = MultilabelAUROC(
+                        num_labels=executor.dataset_collection.label_number
+                    )
+                else:
+                    self.__auroc = MulticlassAUROC(
+                        num_classes=executor.dataset_collection.label_number
+                    )
+        targets = result["targets"]
+        output = self._get_output(result).detach()
+        self.auroc.update(output, targets.detach().long())
+
+    def _after_epoch(self, **kwargs) -> None:
+        epoch = kwargs["epoch"]
+        self._set_epoch_metric(epoch, "AUROC", self.auroc.compute())
