80276da6070edc61f02b275e6d5ffd69be53b717
Meriem30 <m.arbaoui@esi-sba.dz>
2024-08-06 19:40:50 +0200

    more in inferencer.py in ./torch_kit

--- torch_kit/inferencer.py
+++ torch_kit/inferencer.py
@@ -9,42 +9,63 @@ from .typing import ModelGradient
 
 
 class Inferencer(Executor):
+    """
+        Handle inference tasks
+    """
     def inference(
         self,
         evaluation_mode: EvaluationMode = EvaluationMode.Test,
     ) -> bool:
+        # Running inference, with an opt param for specifying the inference phase
+        # a flag to indicate if the inference was successful or not
         succ_flag: bool = False
+        # a flag to determine if gradients are necessary, True unless the mode is Test
         require_grad: bool = EvaluationMode != EvaluationMode.Test
+        # Context managers
         with (
             torch.set_grad_enabled(require_grad),
+            # Set the device context for the model
             self.device_context,
+            # Set the CUDA stream context for asynch. operations
             self.stream_context,
         ):
             try:
+                # Prepare the env and the settings for the execution
                 self._prepare_execution()
+                # Execute one epoch for inference/evaluation
                 self._execute_epoch(epoch=1, evaluation_mode=evaluation_mode)
+                # Execute any hooks that should be executed after inference/evaluation
                 self.exec_hooks(hook_point=ExecutorHookPoint.AFTER_EXECUTE)
+                # If all done, raise a success flag
                 succ_flag = True
             except StopExecutingException:
                 log_warning("stop inference")
             finally:
+                # Ensure that all the operations are completed before returning the succ_flag
                 self.wait_stream()
             return succ_flag
 
     def get_gradient(self) -> ModelGradient:
+        # Perform inference with gradient calculation and return the computed gradients.
         with self.hook_config:
             self.hook_config.disable_log()
+            # A bool var to capture the success status of the inference
             succ: bool = self.inference(
                 evaluation_mode=EvaluationMode.TestWithGrad,
             )
             assert succ
+            # if the inference was successful, return the calculated grads
             return self.model_util.get_gradients()
 
     def get_sample_loss(self, evaluation_mode=EvaluationMode.Test) -> dict:
+        """
+            compute the loss for each sample in the dataset
+            and return a dictionary of these losses
+        """
         sample_loss: dict = {}
         with self.hook_config:
             self.hook_config.disable_log()
-            hook_name = "__cyy_collect_sample_loss"
+            hook_name = "__collect_sample_loss"
             self.append_named_hook(
                 hook_point=ExecutorHookPoint.AFTER_BATCH,
                 name=hook_name,
