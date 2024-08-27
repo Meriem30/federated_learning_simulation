08ff2b39b02f8773dff8c10653fef264f4f388f3
Meriem30 <m.arbaoui@esi-sba.dz>
2024-08-27 12:19:41 +0200

    add BatchLossLogger to global var init

--- torch_kit/metric_visualizers/__init__.py
+++ torch_kit/metric_visualizers/__init__.py
@@ -1,9 +1,11 @@
 from .metric_visualizer import MetricVisualizer
 from .performance_metric_logger import PerformanceMetricLogger
 from .performance_metric_recorder import PerformanceMetricRecorder
+from .batch_loss_logger import BatchLossLogger
 
 __all__ = [
     "MetricVisualizer",
     "PerformanceMetricLogger",
     "PerformanceMetricRecorder",
+    "BatchLossLogger"
 ]
