a6fc0a0d9f8c8962b34499d8209b57f93f9c46d9
Meriem30 <m.arbaoui@esi-sba.dz>
2024-08-27 16:22:35 +0200

    modify ml_type for flexible conf

--- torch_kit/ml_type.py
+++ torch_kit/ml_type.py
@@ -1,4 +1,21 @@
 from enum import StrEnum, auto
+import copy
+from typing import Any, Self
+
+
+class ConfigBase:
+    def __init__(self) -> None:
+        self.__old_config: Any = None
+
+    def __enter__(self) -> Self:
+        self.__old_config = copy.deepcopy(self)
+        return self
+
+    def __exit__(self, *args: Any, **kwargs: Any) -> None:
+        for name in dir(self):
+            if not name.startswith("_"):
+                setattr(self, name, getattr(self.__old_config, name))
+        self.__old_config = None
 
 """
     For each class:
