d999ee3828a3500393bd7d39c600ebcc788b4c31
Meriem30 <m.arbaoui@esi-sba.dz>
2024-09-06 11:06:10 +0200

    add protocols in ./fl_sim_lib and ./fl_sim_lib/worker

---
+++ federated_learning_simulation_lib/protocol.py
@@ -0,0 +1,18 @@
+from typing import Protocol
+
+from .config import DistributedTrainingConfig
+
+
+class ExecutorProtocol(Protocol):
+    @property
+    def hold_log_lock(self) -> bool:
+        ...
+
+    @property
+    def config(self) -> DistributedTrainingConfig:
+        ...
+
+    @property
+    def name(self) -> str:
+        ...
+    
---
+++ federated_learning_simulation_lib/worker/protocol.py
@@ -0,0 +1,34 @@
+from functools import cached_property
+
+from other_libs.topology import Endpoint
+from torch_kit import Trainer
+
+from ..protocol import ExecutorProtocol
+#from ..util import ModelCache
+
+
+class WorkerProtocol(ExecutorProtocol):
+    """
+        define an interface for a worker in distributed learning
+        must have two properties (endpoint and trainer) and define a pause method
+    """
+    @property
+    def endpoint(self) -> Endpoint: ...
+
+    @cached_property
+    def trainer(self) -> Trainer: ...
+
+    def pause(self) -> None: ...
+
+
+class AggregationWorkerProtocol(WorkerProtocol):
+    """
+        extend WorkerProtocol to define additional properties for the server
+        must have the round idx for the current round
+    """
+
+    @property
+    def round_index(self) -> int: ...
+
+    #@property
+    #def model_cache(self) -> ModelCache: ...
