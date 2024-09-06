575f2bdd3559d287095ea2b1dfc27ab5c72c4f7b
Meriem30 <m.arbaoui@esi-sba.dz>
2024-09-06 09:22:47 +0200

    add topology in ./other_libs

---
+++ other_libs/topology/endpoint.py
@@ -0,0 +1,11 @@
+
+from .topology import Topology
+
+
+class Endpoint:
+    def __init__(self, topology: Topology) -> None:
+        self._topology: Topology = topology
+
+    def close(self) -> None:
+        raise NotImplementedError()
+    
