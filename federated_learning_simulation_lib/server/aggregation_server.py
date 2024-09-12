65ef2349e7ae4d46f6e3ea9409833d101284aba6
Meriem30 <m.arbaoui@esi-sba.dz>
2024-09-12 16:59:17 +0200

    more in ./fl_sim_lib/server/aggregation_server

--- federated_learning_simulation_lib/server/aggregation_server.py
+++ federated_learning_simulation_lib/server/aggregation_server.py
@@ -14,32 +14,52 @@ from .round_selection_mixin import RoundSelectionMixin
 
 
 class Server(Executor, RoundSelectionMixin):
+    """
+        extend both Executor and RoundSelectionMixin classes
+        handle communication, selection, evaluation, aggregation
+    """
     def __init__(self, task_id: int, endpoint: ServerEndpoint, **kwargs: Any) -> None:
+        # initialize the server with a given task_id
         name: str = "server"
         if task_id is not None:
             name = f"server of {task_id}"
+        # call both parent constructors
         super().__init__(**kwargs, name=name)
         RoundSelectionMixin.__init__(self)
+        # initialize the server with an endpoint (attribute)
         self._endpoint: ServerEndpoint = endpoint
+        # for model evaluation
         self.__tester: Inferencer | None = None
 
     @property
     def worker_number(self) -> int:
+        """
+            return the number of workers configured with the task
+        """
         return self.config.worker_number
 
     def get_tester(self, copy_tester: bool = False) -> Inferencer:
+        """
+            retrieve or create a tester (Inferencer)
+        """
         if self.__tester is not None and not copy_tester:
             return self.__tester
+        # if copy_tester, create a new one
         tester = self.config.create_inferencer(
             phase=MachineLearningPhase.Test, inherent_device=False
         )
+        # remove the training and the validation dataset from the tester
         tester.dataset_collection.remove_dataset(phase=MachineLearningPhase.Training)
         tester.dataset_collection.remove_dataset(phase=MachineLearningPhase.Validation)
         tester.hook_config.summarize_executor = False
+        # caches the created tester
         self.__tester = tester
         return tester
 
     def load_parameter(self, tester: Inferencer, parameter: ModelParameter) -> None:
+        """
+            load model params into the tester
+        """
         tester.model_util.load_parameters(parameter)
 
     def get_metric(
@@ -48,14 +68,19 @@ class Server(Executor, RoundSelectionMixin):
         log_performance_metric: bool = True,
         copy_tester: bool = False,
     ) -> dict:
+        """
+            evaluate the model (parameter) performance
+        """
         if isinstance(parameter, ParameterMessage):
             parameter = parameter.parameter
+        # configure the tester
         tester = self.get_tester(copy_tester=copy_tester)
         self.load_parameter(tester=tester, parameter=parameter)
         tester.model_util.disable_running_stats()
         tester.hook_config.use_performance_metric = True
         tester.hook_config.log_performance_metric = log_performance_metric
         tester.hook_config.save_performance_metric = False
+        # adjust the batch size
         batch_size: int | None = None
         if "server_batch_size" in tester.dataloader_kwargs:
             batch_size = tester.dataloader_kwargs["server_batch_size"]
@@ -74,18 +99,28 @@ class Server(Executor, RoundSelectionMixin):
             tester.performance_metric.clear_metric()
             metric: dict = tester.performance_metric.get_epoch_metrics(1)
             assert not metric
+        # run inference on the tester
         tester.inference()
+        # retrieve performance metrics
         metric = tester.performance_metric.get_epoch_metrics(1)
         assert metric
+        # free up the resources
         tester.offload_from_device()
         return metric
 
     def start(self) -> None:
+        """
+            the main loop for the server
+            manage the communication with the workers
+        """
+        # set the server context
         ExecutorContext.set_name(self.name)
+        # save the configuration to a file
         with open(os.path.join(self.save_dir, "config.pkl"), "wb") as f:
             pickle.dump(self.config, f)
         self._before_start()
-
+        # check continuously (until _stopped()) for data from worker & process it
+        # remove the worker ID from the copied dataset after being processed
         worker_set: set = set()
         while not self._stopped():
             if not worker_set:
@@ -108,8 +143,9 @@ class Server(Executor, RoundSelectionMixin):
 
             if worker_set and not self._stopped():
                 time.sleep(1)
-
+        # close the endpoint
         self._endpoint.close()
+        # clean up
         self._server_exit()
         log_info("end server")
 
