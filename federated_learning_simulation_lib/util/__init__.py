1f57acff4617545cb89d8048ece788f39cb6d09a
Meriem30 <m.arbaoui@esi-sba.dz>
2024-09-12 09:20:38 +0200

    add util for model cache reset load in ./fl_sim_lib/util

---
+++ federated_learning_simulation_lib/util/__init__.py
@@ -0,0 +1,42 @@
+from typing import Callable, Iterable
+
+import torch.nn
+from other_libs.log import log_debug
+from torch_kit import ModelParameter, Trainer
+
+from .model_cache import *  # noqa: F401
+
+
+def __reset_optimizer_parameters(
+    trainer: Trainer, params: Iterable[torch.nn.Parameter] | None = None
+) -> None:
+    optimizer = trainer.get_optimizer()
+    assert len(optimizer.param_groups) == 1
+    old_param_group = optimizer.param_groups[0]
+    optimizer.param_groups.clear()
+    optimizer.state.clear()
+    if params is None:
+        optimizer.add_param_group({"params": trainer.model.parameters()})
+    else:
+        optimizer.add_param_group({"params": params})
+    for k, v in old_param_group.items():
+        if k not in "params":
+            optimizer.param_groups[0][k] = v
+            log_debug("reuse parameter property %s", k)
+
+
+def load_parameters(
+    trainer: Trainer,
+    parameter: ModelParameter,
+    reuse_learning_rate: bool,
+    loading_fun: Callable | None = None,
+) -> None:
+    if loading_fun is not None:
+        loading_fun(parameter)
+    else:
+        trainer.model_util.load_parameters(parameter)
+    if reuse_learning_rate:
+        __reset_optimizer_parameters(trainer=trainer)
+    else:
+        trainer.remove_optimizer()
+    trainer.model_util.disable_running_stats()
---
+++ federated_learning_simulation_lib/util/model_cache.py
@@ -0,0 +1,56 @@
+import torch
+from other_libs.storage import DataStorage
+from torch_kit import ModelParameter, tensor_to
+
+
+class ModelCache:
+    def __init__(self) -> None:
+        self.__parameter: DataStorage = DataStorage()
+
+    @property
+    def has_data(self) -> bool:
+        return self.__parameter.has_data()
+
+    @property
+    def parameter(self) -> ModelParameter:
+        return self.__parameter.data
+
+    def load_file(self, path: str) -> None:
+        self.__parameter = DataStorage(data_path=path)
+
+    def cache_parameter(self, parameter: ModelParameter, path: str) -> None:
+        self.__parameter.set_data(
+            {
+                k: v.to(dtype=torch.float64)
+                for k, v in tensor_to(parameter, device="cpu").items()
+            }
+        )
+        self.__parameter.set_data_path(path)
+
+    def get_parameter_diff(self, new_parameter: ModelParameter) -> ModelParameter:
+        res = {k: v - self.parameter[k] for k, v in new_parameter.items()}
+        # for k, v in self.parameter.items():
+        #     if not torch.allclose(v + res[k], new_parameter[k]):
+        #         print("key", k)
+        #         print(v + res[k])
+        #         print(v)
+        #         print(new_parameter[k])
+        #         assert False
+        return res
+
+    def add_parameter_diff(self, parameter_diff: ModelParameter, path: str) -> None:
+        self.__parameter.set_data_path(path)
+        for k, v in self.parameter.items():
+            self.parameter[k] = v + tensor_to(parameter_diff[k], device="cpu")
+        self.__parameter.mark_new_data()
+
+    def discard(self) -> None:
+        self.__parameter.clear()
+
+    def save(self) -> None:
+        self.__parameter.save()
+
+    def get_parameter_path(self) -> str:
+        self.__parameter.save()
+        assert self.__parameter.data_path
+        return self.__parameter.data_path
