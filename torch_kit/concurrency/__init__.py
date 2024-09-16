96c106a5313291599838e0fb9e4b60452bda0deb
Meriem30 <m.arbaoui@esi-sba.dz>
2024-09-16 10:49:47 +0200

    import the newly added pytorch context in ./torch_kit/concurrency

--- torch_kit/concurrency/__init__.py
+++ torch_kit/concurrency/__init__.py
@@ -1,5 +1,7 @@
 from .torch_thread_task_queue import TorchThreadTaskQueue
+from .torch_process_context import TorchProcessContext
 
 __all__ = [
+    "TorchProcessContext",
     "TorchThreadTaskQueue",
 ]
