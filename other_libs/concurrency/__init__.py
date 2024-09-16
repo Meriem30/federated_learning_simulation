201e3f95f7be4a471019ce120d7aeb5f24c05af4
Meriem30 <m.arbaoui@esi-sba.dz>
2024-09-16 14:48:28 +0200

    add imports for process handlers in ./other_lib/concurrency and ./torch_kit/concurrency

--- other_libs/concurrency/__init__.py
+++ other_libs/concurrency/__init__.py
@@ -1,4 +1,5 @@
 from .process_context import ManagerProcessContext, ProcessContext
+from .process_pool import ProcessPool
 from .batch import batch_process
 from .task_queue import BatchPolicy, TaskQueue
 from .thread_context import ThreadContext
--- torch_kit/concurrency/__init__.py
+++ torch_kit/concurrency/__init__.py
@@ -1,7 +1,9 @@
 from .torch_thread_task_queue import TorchThreadTaskQueue
 from .torch_process_context import TorchProcessContext
+from .torch_process_pool import TorchProcessPool
 
 __all__ = [
     "TorchProcessContext",
     "TorchThreadTaskQueue",
+    "TorchProcessPool",
 ]
