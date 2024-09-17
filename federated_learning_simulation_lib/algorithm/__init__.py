627c7d82ad5bd244d80a013eff848cdaccac2003
Meriem30 <m.arbaoui@esi-sba.dz>
2024-09-17 11:56:54 +0200

    fix imports in ./fl_sim_lib/algorithm and ./fl_sim_lib/server

--- federated_learning_simulation_lib/algorithm/__init__.py
+++ federated_learning_simulation_lib/algorithm/__init__.py
@@ -1,1 +1,2 @@
-from .aggregation_algorithm import AggregationAlgorithm
+from .aggregation_algorithm import AggregationAlgorithm  # noqa: F401
+from .fed_avg_algorithm import FedAVGAlgorithm  # noqa: F401
