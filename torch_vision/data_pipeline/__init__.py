e205aabc0b0b829d6f1cdf41ae97199591f112f1
Meriem30 <m.arbaoui@esi-sba.dz>
2024-09-23 12:49:19 +0200

    add data pipeline in ./torch_vision/data_pipeline

---
+++ torch_vision/data_pipeline/__init__.py
@@ -0,0 +1,15 @@
+from torch_kit import DatasetType
+from torch_kit.data_pipeline import global_data_transform_factory
+
+from .transform import add_vision_extraction, add_vision_transforms
+
+
+def append_transforms_to_dc(dc, model_evaluator=None) -> None:
+    if model_evaluator is None:
+        add_vision_extraction(dc=dc)
+    else:
+        add_vision_transforms(dc=dc, model_evaluator=model_evaluator)
+    return
+
+
+global_data_transform_factory.register(DatasetType.Vision, append_transforms_to_dc)
