47c34451da2ae6423569c47ed46c57929f0d9577
Meriem30 <m.arbaoui@esi-sba.dz>
2024-09-25 16:51:04 +0200

    [EDIT] move global dataset factory registration from ./torch_vision/dataset/__init__

--- torch_vision/dataset/__init__.py
+++ torch_vision/dataset/__init__.py
@@ -1,8 +1,8 @@
-from torch_kit import DatasetType
-from torch_kit.dataset.util import global_dataset_util_factor
+#from torch_kit import DatasetType
+#from torch_kit.dataset.util import global_dataset_util_factor
 
 from .repository import register_constructors
 from .util import VisionDatasetUtil
 
-global_dataset_util_factor.register(DatasetType.Vision, VisionDatasetUtil)
-register_constructors()
+#global_dataset_util_factor.register(DatasetType.Vision, VisionDatasetUtil)
+#register_constructors()
