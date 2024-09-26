d1c306124c72a2685b051d2aba34a84c54d8410f
Meriem30 <m.arbaoui@esi-sba.dz>
2024-09-26 11:36:36 +0200

    [EDIT] update files ./fl_simulator/simulator.py & test.sh

--- federated_learning_simulator/simulator.py
+++ federated_learning_simulator/simulator.py
@@ -1,13 +1,18 @@
 import os
 import sys
-
 import hydra
-from federated_learning_simulation_lib.config import DistributedTrainingConfig  # import_dependencies
+
+currentdir = os.path.dirname(__file__)
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, os.path.abspath(parentdir))
+
+from federated_learning_simulation_lib.config import DistributedTrainingConfig, import_dependencies
 from federated_learning_simulation_lib.config import load_config as __load_config
 from federated_learning_simulation_lib.training import train
 
 # modify the python path to include the current directory
 sys.path.insert(0, os.path.abspath("."))
+
 import method  # noqa: F401
 
 # initialize a global configuration
@@ -15,13 +20,9 @@ global_config: DistributedTrainingConfig = DistributedTrainingConfig()
 # import_dependencies()
 
 
-# execute this as the main function specifying the config path location
-@hydra.main(config_path="./conf", version_base=None)
+@hydra.main(config_path="./conf", config_name="fed_avg/cifar10.yaml", version_base=None)
 def load_config(conf) -> None:
     global global_config
-    # load a conf file specified by hydra
-    # update the global_config with the loaded conf (param) and
-    # a global YAML conf file located inn the constructed path
     global_config = __load_config(
         conf, os.path.join(os.path.dirname(__file__), "conf", "global.yaml")
     )
@@ -30,5 +31,6 @@ def load_config(conf) -> None:
 if __name__ == "__main__":
     # call the load function
     load_config()
+    print("this is the conf from simulator", global_config.dc_config.dataset_name)
     # start training
     train(config=global_config)
