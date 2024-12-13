17cfe2446a93308ffadb82976ab0b84e87510f15
Meriem30 <m.arbaoui@esi-sba.dz>
2024-12-13 15:41:45 +0100

    [DELETE] separate file for graph worker protocols in ./fl_sim_lib/graph_worker

--- federated_learning_simulation_lib/graph_worker/graph_protocol.py
+++
@@ -1,9 +0,0 @@
-from functools import cached_property
-
-from ..worker.protocol import AggregationWorkerProtocol
-
-
-class GraphWorkerProtocol(AggregationWorkerProtocol):
-
-    @cached_property
-    def training_node_indices(self) -> set: ...
