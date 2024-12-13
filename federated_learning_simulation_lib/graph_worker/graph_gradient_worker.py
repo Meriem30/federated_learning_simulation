00bb614fbfc990fd655e3644cac939b0893704a1
Meriem30 <m.arbaoui@esi-sba.dz>
2024-12-13 15:51:39 +0100

    [EDIT] class initialization in ./fl_sim_lib/graph_worker/gradient_worker

--- federated_learning_simulation_lib/graph_worker/graph_gradient_worker.py
+++ federated_learning_simulation_lib/graph_worker/graph_gradient_worker.py
@@ -1,13 +1,22 @@
-from federated_learning_simulation_lib.worker.gradient_worker import GradientWorker
+from typing import Any
+
+from torch_kit import ModelGradient
+
 from federated_learning_simulation_lib.graph_worker.graph_worker import GraphWorker
+from federated_learning_simulation_lib.worker.gradient_worker import GradientWorker
+from federated_learning_simulation_lib.worker.client import ClientMixin
 # add NodeSelectionMixin
 
 
-class GraphGradientWorker(GraphWorker, GradientWorker):
+class GraphGradientWorker(GraphWorker, GradientWorker, ClientMixin):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def _process_gradient(self, gradient_dict):
-        result = super()._process_gradient(gradient_dict)
+        GraphWorker.__init__(self, **kwargs)
+        GradientWorker.__init__(self, **kwargs)
+        ClientMixin.__init__(self)
+        
+    def _process_gradient(self, gradient_dict: ModelGradient) -> ModelGradient:
+        # send data to the server in a ParameterMessage
+        result = super()._process_gradient(self, gradient_dict)
         # add logic
         return result
+
