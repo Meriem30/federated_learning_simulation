19042bee7c984ba0760bc829461ba900d08fd4d4
Meriem30 <m.arbaoui@esi-sba.dz>
2024-12-16 09:34:00 +0100

    [EDIT] update __init__ file for server in ./fl_sim_lib/server/

--- federated_learning_simulation_lib/server/__init__.py
+++ federated_learning_simulation_lib/server/__init__.py
@@ -1,2 +1,7 @@
 from .aggregation_server import AggregationServer  # noqa: F401
 from .graph_aggregation_server import GraphAggregationServer
+from .node_selection_mixin import NodeSelectionMixin
+from .round_selection_mixin import RoundSelectionMixin
+from .server import Server
+
+__all__ = ["Server", "GraphAggregationServer", "NodeSelectionMixin", "AggregationServer", "RoundSelectionMixin"]
