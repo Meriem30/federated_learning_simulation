7fc47276607b746c433530addbededfe51664970
Meriem30 <m.arbaoui@esi-sba.dz>
2025-01-10 15:54:57 +0100

    [ADD] registir the pneumonia dataset constructor as a a Vision data

--- torch_vision/dataset/repository.py
+++ torch_vision/dataset/repository.py
@@ -4,6 +4,14 @@ import torchvision.datasets
 from other_libs.reflection import get_class_attrs
 from torch_kit import DatasetType
 from torch_kit.dataset.repository import register_dataset_constructors
+from .pneumonia_data import PneumoniaDataset
+from torchvision import transforms
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
 
 def register_constructors() -> None:
@@ -19,3 +27,10 @@ def register_constructors() -> None:
 
     for name, constructor in dataset_constructors.items():
         register_dataset_constructors(DatasetType.Vision, name, constructor)
+    register_dataset_constructors(DatasetType.Vision,"PNEUMONIA",
+                                  lambda: PneumoniaDataset(
+                                      csv_file=f"C:/Users/Home/pytorch_dataset/PNEUMONIA/img__data.csv",
+                                      image_dir="C:/Users/Home/pytorch_dataset/PNEUMONIA/stage_2_train_images/",
+                                      transform=transform,
+                                  ))
+
