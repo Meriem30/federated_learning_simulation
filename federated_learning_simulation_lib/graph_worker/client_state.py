46960ef471713f346507d4b815ca6e92e9de2c0d
Meriem30 <m.arbaoui@esi-sba.dz>
2025-02-14 12:38:25 +0100

    [UPDATE] client_state class to include Mutual Information(mi) prop

--- federated_learning_simulation_lib/graph_worker/client_state.py
+++ federated_learning_simulation_lib/graph_worker/client_state.py
@@ -6,38 +6,20 @@ from other_libs.log import log_debug
 class ClientState:
     def __init__(self, worker_id: int) -> None:
         self._client_id = worker_id
-        self._memory_occupation: float = random.uniform(1,0.1)
-        self._energy_consumption: float = random.uniform(1,0.1)
-        self._battery: int = random.random()
+        self._mi: float = 0.0
         self._family: int = 0
 
     def initialize_state(self, families: int) -> None:
-        self._battery = random.uniform(50, 100)
-        self._energy_consumption = random.uniform(1.0, 100.0)
-        self._memory_occupation = random.uniform(1.0, 1000.0)
         self._family = random.choice(list(range(1, families)))
         log_debug(f"Initialized ClientState: {self}")
 
-    def update_state(self, new_battery: float, new_energy: float, new_memory: float, family: int) -> None:
-        self._battery = new_battery
-        self._energy_consumption = new_energy
-        self._memory_occupation = new_memory
+    def update_state(self, new_battery: float, new_energy: float, new_memory: float, new_mi: float, family: int) -> None:
+        self._mi = new_mi
         self._family = family
 
     @property
-    def node_id(self) -> int:
-        return self._client_id
-    @property
-    def battery(self) -> float:
-        return self._battery
-
-    @property
-    def energy_consumption(self) -> float:
-        return self._energy_consumption
-
-    @property
-    def memory_occupation(self) -> float:
-        return self._memory_occupation
+    def mi(self):
+        return self._mi
 
     @property
     def family(self):
@@ -48,28 +30,25 @@ class ClientState:
         return len([attr for attr in vars(ClientState(0)) if
                     not attr.startswith('_client_id') and not attr.startswith('_family')])
 
-    def set_family(self, family_id: int):
+    def set_family(self, family_id: int) -> None:
         self._family = family_id
 
+    def set_mi(self, mutual_information: float) -> None:
+        self._mi = mutual_information
+
     def __repr__(self):
-        return (f"{self._client_id}:"
-                         f"(battery={self._battery},"
-                         f"energy_consumption={self._energy_consumption},"
-                         f" memory_occupation={self._memory_occupation},"
-                         f"family={self._family})")
+        return (f"{self._client_id}:\n"
+                f"mutual_information={self._mi}\n"
+                f"family={self._family})\n")
 
     def to_dict(self) -> dict:
         return {
             "client_id": self._client_id,
-            "battery": self._battery,
-            "energy_consumption": self._energy_consumption,
-            "memory_occupation": self._memory_occupation,
+            "mi": self._mi,
             "family": self._family
         }
 
     def from_dict(self, state_dict: dict) -> None:
         self._client_id = state_dict.get("client_id", self._client_id)
-        self._battery = state_dict.get("battery", self._battery)
-        self._energy_consumption = state_dict.get("energy_consumption", self._energy_consumption)
-        self._memory_occupation = state_dict.get("memory_occupation", self._memory_occupation)
+        self._mi = state_dict.get("mi", self._mi)
         self._family = state_dict.get("family", self._family)
