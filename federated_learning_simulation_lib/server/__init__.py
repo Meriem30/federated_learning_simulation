6461a3166eb4135b8154eb3507255999542e7d45
Meriem30 <m.arbaoui@esi-sba.dz>
2025-12-15 16:30:07 +0100

    [ADD] missing content of algo1 & algo2_utils and fix extensions

--- federated_learning_simulation_lib/server/__init__.py
+++ federated_learning_simulation_lib/server/__init__.py
@@ -3,5 +3,7 @@ from .graph_aggregation_server import GraphAggregationServer
 from .node_selection_mixin import NodeSelectionMixin
 from .round_selection_mixin import RoundSelectionMixin
 from .server import Server
+from .clustered_sampling_server_algo2 import ClusteredSamplingServerAlgo2
+from .clustered_sampling_server_algo1 import ClusteredSamplingServerAlgo1
 
-__all__ = ["Server", "GraphAggregationServer", "NodeSelectionMixin", "AggregationServer", "RoundSelectionMixin"]
+__all__ = ["Server", "GraphAggregationServer", "NodeSelectionMixin", "AggregationServer", "RoundSelectionMixin", "ClusteredSamplingServerAlgo2", "ClusteredSamplingServerAlgo1"]
